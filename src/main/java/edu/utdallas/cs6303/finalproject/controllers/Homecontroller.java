26a0844dae87c3e34e32a71e8d7b5de20043810b
Ryan Appel <leppanayr@yahoo.com>
2020-09-09 16:15:25 -0500

    updating site for basic login

--- src/main/java/edu/utdallas/cs6303/finalproject/controllers/Homecontroller.java
+++ src/main/java/edu/utdallas/cs6303/finalproject/controllers/Homecontroller.java
@@ -1,5 +1,24 @@
 package edu.utdallas.cs6303.finalproject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class Homecontroller {
+    @GetMapping("")
+    public String Index(Model model) {
+        model.addAttribute("myValue","Hello world!");
+        model.addAttribute("mySecondValue","Hello Second world!");
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String Index() {
+        return "login";
+    }
+
     
 }
